53ff195acd2f7d8f84beef9a09f0acb98caec4db
Eas <me@eas.web.id>
2020-09-18 03:20:57 +0700

    add dynamic port

--- server/app.js
+++ server/app.js
@@ -5,8 +5,10 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http); // currying function
 const socket = require('./config/socket')
 
+const PORT = process.env.PORT || 3000
+
 socket.start(io)
 
-http.listen(3000, () => {
+http.listen(PORT, () => {
 	console.log("listening on *:3000");
 });
